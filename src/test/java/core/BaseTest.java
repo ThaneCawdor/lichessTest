84787fffe05df74c46f44c451965feeab7af8a1d
ThaneCawdor <gavacat@icloud.com>
2022-07-13 16:04:06 +0300

    refactoring

--- src/test/java/core/BaseTest.java
+++ src/test/java/core/BaseTest.java
@@ -3,24 +3,24 @@ package core;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 
 abstract public class BaseTest {
-    public void setUp(){
+    public static void setUp(){
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
         Configuration.driverManagerEnabled = true;
         Configuration.browserSize = "1920x1080";
         Configuration.headless = false;
     }
-    @Before
-    public void init(){
+    @BeforeEach
+    void init(){
         setUp();
     }
 
-    @After
-    public void tearDown(){
+    @AfterEach
+    void tearDown(){
         Selenide.closeWebDriver();
     }
 }
